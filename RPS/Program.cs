168d77f9347a29ce0f49e2a8c596b049a5acf9fc
niklashog <hogstromniklas@gmail.com>
2024-11-14 14:34:15 +0100

    Add project files.

---
+++ RPS/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace RPS
+{
+    internal class Program
+    {
+        /// <summary>
+        /// 
+        /// Titta på denna video. Gör sedan samma refactoring.
+        /// https://www.youtube.com/watch?v=cCSrPZroICg
+        /// 
+        /// </summary>
+        /// <param name="args"></param>
+
+        static void Main(string[] args)
+        {
+            GameManager gm = new GameManager();
+
+            do
+            {
+                RoundResult result = gm.PlayRound();
+                if (result == RoundResult.Player1Win)
+                {
+                    Console.WriteLine("Player 1 Wins!");
+                }
+                else if (result == RoundResult.Player2Win)
+                {
+                    Console.WriteLine("Player 2 Wins!");
+                }
+                else
+                {
+                    Console.WriteLine("It's a draw!");
+                }
+                Console.Write("Play Again (Y/N)? ");
+            } while (Console.ReadLine().ToUpper() == "Y");
+        }
+    }
+}
